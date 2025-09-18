7a8063fd3cd0e766321d362c64a81d3c2b0cda89
Nickspero <nspappas99@gmail.com>
2025-09-17 20:01:43 -0400

    first commit

--- src/components/UI/Book.jsx
+++ src/components/UI/Book.jsx
@@ -5,30 +5,8 @@ import Price from "./Price";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Book = ({ book }) => {
-  const [img, setImg] = useState();
-
-  const mountedRef = useRef(true);
-
-  useEffect(() => {
-    const image = new Image();
-    image.src = book.url;
-    image.onload = () => {
-      setTimeout(() => {
-        if (mountedRef.current) {
-          setImg(image);
-        }
-      }, 300);
-    };
-
-    return () => {
-      mountedRef.current = false;
-    };
-  }, [book.url]);
-
   return (
     <div className="book">
-      {!img ? (
-        <>
           <Link to={`/books/${book.id}`}>
             <figure className="book__img--wrapper">
               <img src={book.url} alt="" className="book__img" />
@@ -44,16 +22,6 @@ const Book = ({ book }) => {
             salePrice={book.salePrice}
             originalPrice={book.originalPrice}
           />
-        </>
-      ) : (
-        <>
-          {" "}
-          <div className="book__img--skeleton"></div>
-          <div className="skeleton book__title--skeleton"></div>
-          <div className="skeleton book__rating--skeleton"></div>
-          <div className="skeleton book__price--skeleton"></div>
-        </>
-      )}
     </div>
   );
 };
